47d903a10df4571eb8bd1a22d2d76a0c890e583a
blltrx <blltrx@roseis.gay>
2025-07-21 11:18:17 +0100

    use regex to match indeterminate length tag

--- main.py
+++ main.py
@@ -44,20 +44,20 @@ if "-h" in argvars or "--help" in argvars or "-help" in argvars:
     print(HELP_MESSAGE)
     sys.exit(0)
 
-
 def addHashMarkToHTML(htmlstring: str) -> str:
     """return htmlstring but with decorative element
     added infront of the first instance of heading 1"""
     try:
-        h1index = htmlstring.index("<h1>")
+        h1start, h1end = re.compile("<h1.*?>").search(htmlstring).span()
         HASHMARKSTRING = '<h1 class="title"><span class="hash">#</span>'
         (htmlstring) = (
-            htmlstring[:h1index] +
+            htmlstring[:h1start] +
             HASHMARKSTRING +
-            htmlstring[h1index+4:])
-        # 4 is the length of the substring
+            htmlstring[h1end:])
         return htmlstring
-    except ValueError:
+    except ValueError as exception:
+        if DEBUG:
+            print(f"[DEBUG]: hashmark fail with {exception}")
         return htmlstring
 
 
